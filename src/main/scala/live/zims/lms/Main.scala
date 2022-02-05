42cacfb34323f181e182a1c36e42b9e2fdefefe6
Mohammad Yousuf Minhaj Zia <yzia2000@gmail.com>
2022-02-06 00:32:21 +0800

    feat: created base scala project
    sbt new devinsideyou/scala3-seed.g8

---
+++ src/main/scala/live/zims/lms/Main.scala
@@ -0,0 +1,9 @@
+package live.zims
+package lms
+
+@main def Main(args: String*): Unit =
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
---
+++ src/test/scala/live/zims/lms/ExampleSuite.scala
@@ -0,0 +1,7 @@
+package live.zims
+package lms
+
+final class ExampleSuite extends TestSuite:
+  test("hello world") {
+    1 `shouldBe` 1
+  }
